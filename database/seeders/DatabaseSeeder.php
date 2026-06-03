af8a282a4e106c5044a49c515cd7293598a6d0da
bdavimel15 <bdavi6390@gmail.com>
2026-06-03 17:11:27 -0300

    fix: build frontend assets on railway

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SegmentoClienteCampoSeeder::class,
             SegmentoPresetSeeder::class,
+            MockClienteSeeder::class,
         ]);
     }
 }
